08fd9bc61da3ef4beeb95a2ff2025aaead257d4d
basualdofelipe <basualdofelipe@gmail.com>
2022-08-10 23:57:43 -0300

    config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,9 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  /*
-  INSERTAR KEY
- */
+  apiKey: "AIzaSyDIeJziQb9ZjB25PJk8BMxDpJdHMhQyV78",
+  authDomain: "curso-react-11e9b.firebaseapp.com",
+  projectId: "curso-react-11e9b",
+  storageBucket: "curso-react-11e9b.appspot.com",
+  messagingSenderId: "786664734735",
+  appId: "1:786664734735:web:43dc44e0036cf36edce37a"
 };
 
 // Initialize Firebase
